ad1fdb2710156d1898c402e74cea2e52c0dfb8ee
Maxime <132160016+ShikiTsugu2@users.noreply.github.com>
2025-02-14 10:12:36 +0100

    ajout logo

--- chatbot_app.py
+++ chatbot_app.py
@@ -2,7 +2,11 @@ import streamlit as st
 from chatbot_module import respond_to_user
 
 # Titre de l'application
-st.title("Chatbot Ravenfox")
+col1, col2, col3 = st.columns([1, 6, 1])
+with col1:
+    st.image("logo.jpeg", width=100)
+with col2:
+    st.title("Chatbot Ravenfox")
 
 # Champ de saisie pour la question de l'utilisateur
 user_query = st.text_input("Posez votre question:")
